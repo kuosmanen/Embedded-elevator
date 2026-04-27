c93966f772d0c5d0fd399bd4b7ed7b6c19d2be55
janerikmerio123 <128383939+janerikmerio123@users.noreply.github.com>
2026-04-27 20:43:11 +0300

    Update mega_twi.h
    editing comment

--- Elevator_Project/Elevator_Project/mega_twi.h
+++ Elevator_Project/Elevator_Project/mega_twi.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 
 void twi_master_init(void); //initializing Mega (master)
-void twi_master_send_byte(uint8_t slave_address, uint8_t data); // // MEGA (master) communicates with UNO (slave) aka. sends data
+void twi_master_send_byte(uint8_t slave_address, uint8_t data); //MEGA (master) communicates with UNO (slave) aka. sends data
 
 #endif
